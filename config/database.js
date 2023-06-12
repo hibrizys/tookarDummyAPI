ec0129a8dec334925d00e10e58e7bf11a6114714
Hibrizy Sulistiyo <hibrizys@gmail.com>
2023-06-13 06:05:23 +0700

    Dummy Finished

---
+++ config/database.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+console.log("halohalo");
+console.log(process.env.DB_HOST); //
+
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10, // Adjust as needed
+  host: process.env.DB_HOST, //
+  user: process.env.DB_USER, //
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+});
+
+// Test the database connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to the database');
+    connection.release();
+  }
+});
+
+
+// Make the database connection available in other modules (if needed)
+module.exports = pool;
